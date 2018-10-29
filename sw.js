aaccc98493d5f6b7972074dab41f3f441660cd8b
gefy <gefferson@attornatus.com.br>
2018-10-29 01:12:32 -0300

    add service worker

---
+++ sw.js
@@ -0,0 +1,52 @@
+importScripts('/cache-polyfill.js');
+
+var CACHE_NAME = 'gefy-cache-v1';
+
+
+self.addEventListener('install', event => {
+  this.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/countdown/',
+          '/countdown/style.css',
+          '/_sass/minima.scss',
+          '/countdown/beep.mp3',
+          '/countdown/finish-beep.mp3'
+        ]);
+      })
+  );
+});
+
+this.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => {
+            (cacheName.startsWith('gefy-'))
+          })
+          .filter(cacheName => {
+            (cacheName !== staticCacheName)
+          })
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+this.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/offline/index.html');
+      })
+  )
+});
