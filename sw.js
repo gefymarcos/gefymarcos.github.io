2eff0a34289ae16864b1ae1eb2996f107a7125d1
gefy <gefferson@attornatus.com.br>
2018-11-01 22:18:51 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-var filesToAdd = ['/',
-  '/index.html',
-  '/img/',
-  '/countdown/',
-  '/countdown/style.css',
-  '/css/main.css',
-  '/countdown/beep.mp3',
-  '/countdown/finish-beep.mp3',
-]
-
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
 //Install stage sets up the offline page in the cache and opens a new cache
@@ -19,7 +9,14 @@ var preLoad = function(){
   console.log('[PWA Builder] Install Event processing');
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Cached index and offline page during Install');
-    return cache.addAll(filesToAdd);
+    return cache.addAll(['/',
+    '/index.html',
+    '/img/',
+    '/countdown/',
+    '/countdown/style.css',
+    '/css/main.css',
+    '/countdown/beep.mp3',
+    '/countdown/finish-beep.mp3']);
   });
 }
 
